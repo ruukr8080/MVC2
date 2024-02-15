908c8573ee321d2adc29eae228d49684e1c21741
ruukr8080 <ruukr8080@gmail.com>
2024-02-15 16:25:43 +0900

    first commit

---
+++ src/test/java/thymeleaf/mvc2/Mvc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package thymeleaf.mvc2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mvc2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
